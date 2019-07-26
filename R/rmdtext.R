d32284f10a350a99d9f0a2f5e3cc24262b4d932b
Zhibin <9056380+ningzhibin@users.noreply.github.com>
2019-07-26 16:05:07 -0400

    dd

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -1,6 +1,12 @@
 ## This app requires OpenCPU 1.0.1 or higher !!!!
 ## This is just a test for how rmd works on the ocpu server end
 
+
+# the rendering function needs to right a temperary file, whether in the same path as the input, or defined by the user
+# if the input is on the server, usually the path is not writable, therefore you need to set the intermediate_dir
+# an workround way is to readin the file, and write into a temp file. therefore everything is going to be in the temp file
+
+
 #' @export
 #'
 render_rmd_url <- function(rmd_url){
@@ -13,8 +19,6 @@ render_rmd_url <- function(rmd_url){
 }
 
 
-# the rendering function needs to right a temperary file, whether in the same path as the input, or defined by the user
-# if the input is on the server, usually the path is not writable, therefore you need to set the intermediate_dir
 
 
 render_rmd_server <- function(rmd_file){
@@ -23,7 +27,6 @@ render_rmd_server <- function(rmd_file){
 
   tempfolder <-  tempdir()
   rmarkdown::render(path_to_input, output_format = "html_document",
-                    #output_dir = tempfolder,
                     intermediates_dir = tempfolder, # the ocpu server does nothave
                     output_file = "output.html")
 
