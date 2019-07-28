8e8535db7c3b2bd9a9a0badebc00ff8d17c9a90b
Zhibin <9056380+ningzhibin@users.noreply.github.com>
2019-07-27 20:05:25 -0400

    0.1.4

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -1,14 +1,44 @@
-## This app requires OpenCPU 1.0.1 or higher !!!!
-## This is just a test for how rmd works on the ocpu server end
-
-
-# the rendering function needs to right a temperary file, whether in the same path as the input, or defined by the user
-# if the input is on the server, usually the path is not writable, therefore you need to set the intermediate_dir
-# an workround way is to readin the file, and write into a temp file. therefore everything is going to be in the temp file
-
+#' wrapper function of rmarkdown::render for opencpu
+#'
+#' Three general wrapper function to work with opencpu, which can render source r markdown code into html files, to be displacyed on the client end
+#'
+#' @param text For rmdtext, a string, which is the rmardown coding itself
+#' @return no direct return, but write an output.html to the temp session on the opencpu server
+#'
+#' @seealso \code{\link{render}}  \code{\link{knit}}
+#'
+#' @examples
+#'   path_to_input <- system.file("rmd", rmd_file, package = "rmdocpu")
+#'   myfile <- readLines(path_to_input)
+#'   rmdtext(myfile)
 
 #' @export
 #'
+#'
+#'
+rmdtext <- function(text){
+  writeLines(text, con="input.Rmd");
+  rmarkdown::render("input.Rmd", output_file="output.html");
+  invisible();
+}
+
+
+#' wrapper function of rmarkdown::render for opencpu
+#'
+#' Three general wrapper function to work with opencpu, which can render source r markdown code into html files, to be displacyed on the client end
+#'
+#' @param rmd_url For render_rmd_url, a string of public accessible url
+#'
+#' @return no direct return, but write an output.html to the temp session on the opencpu server
+#' @seealso \code{\link{render}}  \code{\link{knit}}
+#' @examples
+#'   render_rmd_url("https://raw.githubusercontent.com/ningzhibin/metalab/master/inst/rmd/input.Rmd")
+#' @export
+#'
+#'
+#'
+
+
 render_rmd_url <- function(rmd_url){
   ## for test rmd_url https://raw.githubusercontent.com/ningzhibin/metalab/master/inst/rmd/input.Rmd
   myfile <- RCurl::getURL(rmd_url)
@@ -19,7 +49,18 @@ render_rmd_url <- function(rmd_url){
 }
 
 
-
+#' wrapper function of rmarkdown::render for opencpu
+#'
+#' Three general wrapper function to work with opencpu, which can render source r markdown code into html files, to be displacyed on the client end
+#'
+#' @param rmd_file For render_rmd_server, a string of the file name on the same opencpu server in the same package, only for tests
+#' @return no direct return, but write an output.html to the temp session on the opencpu server
+#' @seealso \code{\link{render}}  \code{\link{knit}}
+#' @examples
+#'   path_to_input <- system.file("rmd", rmd_file, package = "rmdocpu")
+#'   render_rmd_server(path_to_input)
+#' @export
+#'
 
 render_rmd_server <- function(rmd_file){
   ## for test use input.Rmd
@@ -29,19 +70,10 @@ render_rmd_server <- function(rmd_file){
   writeLines(myfile, con="input.Rmd");
   rmarkdown::render("input.Rmd",output_format = "html_document", output_file="output.html")
 
-  #tempfolder <-  tempdir()
-  #rmarkdown::render(path_to_input, output_format = "html_document",
-  #                  intermediates_dir = tempfolder, # the ocpu server does nothave
-  #                  output_file = "output.html")
-
-
   invisible()
 }
 
-
-rmdtext <- function(text){
-  writeLines(text, con="input.Rmd");
-  rmarkdown::render("input.Rmd", output_file="output.html");
-  invisible();
-}
+# the rendering function needs to right a temperary file, whether in the same path as the input, or defined by the user
+# if the input is on the server, usually the path is not writable, therefore you need to set the intermediate_dir
+# an workround way is to read in the file, and write into a temp file. therefore everything is going to be in the temp file
 
