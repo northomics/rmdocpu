d155537a48c185321a20139bd3eef382cd01d061
Zhibin <9056380+ningzhibin@users.noreply.github.com>
2019-08-01 13:23:43 -0400

    maquant summary file

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -49,6 +49,59 @@ render_rmd_url <- function(rmd_url){
 }
 
 
+
+#' wrapper function of rmarkdown::render for opencpu to render a report for maxquant summary
+#'
+#' This function works on the opencpu server end, to produce an html file to send to the front end to display.
+#' It also works on the stand alone mode, to generate a report file, output.html in the currrent folder, using a public accessible rmd file on github
+#'
+#' @param data_table the table is a matrix/data.frame/tble on the server ond, json formted on the front ond opencpu will do the conversion automatically
+#'
+#' @return no direct return, but write an output.html to the temp session on the opencpu server
+#' @seealso \code{\link{render}}  \code{\link{knit}}
+#' @examples
+#'   render_rmd_MQ_summary()
+#' @export
+#'
+#'
+#'
+
+
+render_rmd_MQ_summary <- function(data_table){
+  ## for test rmd_url https://raw.githubusercontent.com/ningzhibin/rmdocpu/master/inst/rmd/MQ_report_summary_ocpu.Rmd
+  myfile <- RCurl::getURL("https://raw.githubusercontent.com/ningzhibin/rmdocpu/master/inst/rmd/MQ_report_summary_ocpu.Rmd")
+  writeLines(myfile, con="input.Rmd");
+  rmarkdown::render("input.Rmd",output_format = "html_document", params = list(summary_file_tbl =  data_table), output_file="output.html")
+  invisible()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #' wrapper function of rmarkdown::render for opencpu
 #'
 #' Three general wrapper function to work with opencpu, which can render source r markdown code into html files, to be displacyed on the client end
@@ -72,6 +125,29 @@ render_rmd_server <- function(rmd_file){
 
   invisible()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # the rendering function needs to right a temperary file, whether in the same path as the input, or defined by the user
 # if the input is on the server, usually the path is not writable, therefore you need to set the intermediate_dir
